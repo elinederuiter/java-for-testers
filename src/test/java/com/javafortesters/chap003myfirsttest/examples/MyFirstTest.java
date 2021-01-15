e143aa817c7041178984744d29eb22c2057020ec
Eline De ruiter <elinederuiter@MacBook-Air-van-Eline.local>
2021-01-15 18:01:36 +0100

    message

---
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -0,0 +1,10 @@
+package com.javafortesters.chap003myfirsttest.examples;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+    @Test
+    void create() {
+        Calculator calc =
+    }
+}
