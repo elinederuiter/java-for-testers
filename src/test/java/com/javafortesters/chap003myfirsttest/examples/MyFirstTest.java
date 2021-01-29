48765f1e38993448ca895d3a9d7bd7980d229a69
Eline De ruiter <elinederuiter@MacBook-Air-van-Eline.local>
2021-01-29 16:49:05 +0100

    My first test

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -1,10 +1,14 @@
 package com.javafortesters.chap003myfirsttest.examples;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyFirstTest {
     @Test
-    void create() {
-        Calculator calc =
+    public void canAddTwoPlusTwo() {
+        int answer = 2 + 2;
+        assertEquals(4, answer,"2+2=4");
     }
 }
+
+
