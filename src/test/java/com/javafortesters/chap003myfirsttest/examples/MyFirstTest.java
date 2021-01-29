0a1eda77aba2001121fb29c374f119cc4801bd63
Eline De ruiter <elinederuiter@MacBook-Air-van-Eline.local>
2021-01-29 17:04:52 +0100

    downgrade to junit4

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -1,13 +1,15 @@
 package com.javafortesters.chap003myfirsttest.examples;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MyFirstTest {
     @Test
     public void canAddTwoPlusTwo() {
         int answer = 2 + 2;
-        assertEquals(4, answer,"2+2=4");
+        assertEquals("2+2=4",4, answer);
     }
 }
 
